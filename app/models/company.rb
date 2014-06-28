e7c35610aef987c978515910abdae65a30414187
xdhe <hexudong08@gmail.com>
2014-06-29 02:38:09 +0800

    updated

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,32 @@
 class Company < ActiveRecord::Base
+
   mount_uploader :logo, LogoUploader
   attr_accessible :address, :email, :logo, :name, :phone
+
+  def t(key)
+    I18n.t("activerecord.attributes.company.#{key.to_s}")
+  end
+
+  def logo_viewer
+    ActionController::Base.helpers.image_tag(self.logo.url)
+  end
+
+  def method_missing(name, *args)
+    method_name = name.to_s
+    if method_name['viewer'] && self.class.column_names.include?(method_name.split("_").first)
+      send(method_name.split("_").first)
+    else
+      super
+    end
+  end
+
+  class << self
+    def t(key)
+      I18n.t("activerecord.attributes.company.#{key.to_s}")
+    end
+
+    def serialize_columns
+      [:name, :phone, :email, :address, :logo]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Cms::Application.routes.draw do
   match "home/work"  => "home#work"
   match "home/intro" => "home#intro"
   match "home/idea"  => "home#idea"
+  match "admin"      => "companies#index", :module => :admin
 
   root :to => "home#index"
 end
