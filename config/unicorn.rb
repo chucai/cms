d73009afef7ec745a28e0b6d8500999dc335589b
xdhe <hexudong08@gmail.com>
2014-10-16 15:09:14 +0800

    unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/www/designdot/current"
+pid "/var/www/designdot/current/tmp/pids/unicorn.pid"
+stderr_path "/var/www/designdot/current/log/unicorn.log"
+stdout_path "/var/www/designdot/current/log/unicorn.log"
+listen "/var/www/designdot/current/tmp/unicorn.designdot.sock"
+
+worker_processes 2
+timeout 30
